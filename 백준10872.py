1b183b38d5962fe45159f2fdc676acd937c05a2d
VictoryJu <68391427+VictoryJu@users.noreply.github.com>
2020-08-11 23:59:02 +0900

    Update 백준10872.py
    0일때를 고려안해줘서 런타임 에러가 났다.

--- 백준10872.py
+++ 백준10872.py
@@ -1,12 +1,12 @@
 import sys
 
 def factorial(num):
-    if num == 1:
-        return num
+    if num == 1 or num == 0:
+        return 1
     else: 
         return num * factorial(num-1)
 
 a = int(sys.stdin.readline())
-fac = factorial(a)
+print(factorial(a))
 
-print(fac)
+
