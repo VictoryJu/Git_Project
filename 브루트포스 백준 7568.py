ea37a889171e0f474292d0f5d879155ea3cfbd1c
VictoryJu <68391427+VictoryJu@users.noreply.github.com>
2020-08-19 19:04:59 +0900

    Update 브루트포스 백준 7568.py

--- 브루트포스 백준 7568.py
+++ 브루트포스 백준 7568.py
@@ -1,24 +1,21 @@
-
-x = []
-y = []
-rank = []
-
-n=int(input())
-
-for i in range(0,n):    
-    x,y = map(int,input().split())
-    
-
-for j in range(n):
-    num1 = n
-    for z in range(n):
-        if x[j] > x[z] and y[j] > y[z]:
-            rank[j] = num1+0
-        elif x[j] > x[z] and y[j] < y[z]:
-            rank[j] = num1-1
-        elif x[j] < x[z] and y[j] > y[z]:
-            rank[j] = num1-1
-
+#만일 자신보다 더 큰 덩치의 사람이 k명이라면 그 사람의 덩치 등수는 k+1이 된다.
+# 3명 기준 0번이랑 1 2 비교 1번이랑 0 2 비교 2번이랑 0 3 비교
 i=0
+W = []
+H = []
+rank = []
+num = 0
+n = int(input())
+for i in range(n):
+    W.append(int(input()))
+    H.append(int(input()))
+
+for i in range(n):
+    num = 0
+    for j in range(n):
+        if W[i] < W[j]:
+            rank[i] = num + 1
+        
 for i in range(n):
     print(rank[i])
+            
