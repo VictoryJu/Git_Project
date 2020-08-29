bd6079dc2f819da1dfa0576368b84f5104bb3b80
VictoryJu <68391427+VictoryJu@users.noreply.github.com>
2020-08-29 21:13:38 +0900

    Update 브루트포스 백준 7568.py
    등수 구하는 for문 부분 어떻게 돌아가는지 이해가 잘안됨 공부를 더 해봐야 할거같음

--- 브루트포스 백준 7568.py
+++ 브루트포스 백준 7568.py
@@ -1,19 +1,18 @@
 #만일 자신보다 더 큰 덩치의 사람이 k명이라면 그 사람의 덩치 등수는 k+1이 된다.
 # 3명 기준 0번이랑 1 2 비교 1번이랑 0 2 비교 2번이랑 0 3 비교
 i=0
-W = []
-H = []
+people = []
 
 n = int(input())
 
 for i in range(n):
-    W.append(int(input()))
-    H.append(int(input()))
+   a,b = map(int,input().split())
+   people.append((a,b))
 
-for i in range(n):
+for i in people:
     rank = 1
-    for j in range(n):
-        if W[i] < W[j] and H[i] < H[j]:
+    for j in people:
+        if i[0] < j[0] and i[1] < j[1]:
             rank += 1
     print(rank)    
         
